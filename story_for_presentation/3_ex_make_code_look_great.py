bc8963a9106889c89b9b51b305ed39208d24d9a6
flrs <9593883+flrs@users.noreply.github.com>
2019-11-20 22:13:40 -0800

    Worsen code formatting in bad code format example

--- story_for_presentation/3_ex_make_code_look_great.py
+++ story_for_presentation/3_ex_make_code_look_great.py
@@ -1,4 +1,4 @@
-people_on_meetup = [
+people_on_meetup  =   [
 'A tiny horse' ,
   'Mystic Mouse',
             'Steg O Saurus',
@@ -6,13 +6,13 @@ people_on_meetup = [
 ]
 
 class Meetup:
- def __init__ ( self, members):
+ def   __init__ ( self, members ):
         self.members =        members
-    def count_members(       self):
-   return len(self.members)
+    def   count_members(  self ):
+   return len( self.members )
 
 if __name__ == '__main__':
-  this_meetup = Meetup(people_on_meetup)
+  this_meetup = Meetup  ( people_on_meetup      )
 
     print( 'Hello, Pythonistas!')
     print('We are a great group of {}.'.format(
